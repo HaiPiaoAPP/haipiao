22a9d19c1311c1eaa713acc2e191c3210bcf8517
Zhongyi Fan <zhongyifan@ZhongyideMacBook-Pro.local>
2019-05-19 13:29:09 -0700

    set up jpa

---
+++ persist/src/test/java/unit/com/haipiao/persist/repository/UserRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.haipiao.persist.repository;
+
+import com.haipiao.persist.config.PersistConfig;
+import com.haipiao.persist.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {PersistConfig.class})
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testSaveAndReadById() throws Exception {
+        User user = new User();
+        userRepository.findById(userRepository.save(user).getUserId());
+
+    }
+
+}
